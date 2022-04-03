047a6863d71d104ff0d6fd70fb36c256f4332bde
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-04-03 22:50:55 +0900

    add user spec

--- server/spec/factories/users.rb
+++ server/spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :user do
-    email { 'MyString' }
+    sequence(:email) { |i| "test#{i}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
   end
--- server/spec/models/user_spec.rb
+++ server/spec/models/user_spec.rb
@@ -5,5 +5,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'shoulda mathcers' do
     it { is_expected.to have_secure_password }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:email).is_at_most(255) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+    it do
+      FactoryBot.create(:user)
+      is_expected.to validate_uniqueness_of(:email).case_insensitive
+    end
   end
 end
