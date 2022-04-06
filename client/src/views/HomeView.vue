a693f35be11bb222dba463326d8ea6a7392f3e51
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-04-06 23:59:30 +0900

    add userCreate to client

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>home</h1>
+  <SignUp />
 </template>
+
+<script setup lang="ts">
+import SignUp from "./default/SignUp.vue";
+</script>
---
+++ client/src/views/default/SignUp.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="container">
+    <input type="email" placeholder="email" v-model="signupParams.email" />
+    <input
+      type="password"
+      placeholder="password"
+      v-model="signupParams.password"
+    />
+    <input
+      type="password"
+      placeholder="password confirmation"
+      v-model="signupParams.passwordConfirmation"
+    />
+    <input type="submit" @click="userCreate()" />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+import { useUserCreateMutation } from "@/generated/graphql";
+
+const signupParams = reactive({
+  email: "",
+  password: "",
+  passwordConfirmation: "",
+});
+
+const { mutate: userCreate } = useUserCreateMutation(() => ({
+  variables: {
+    email: signupParams.email,
+    password: signupParams.password,
+    passwordConfirmation: signupParams.passwordConfirmation,
+  }
+}))
+</script>
