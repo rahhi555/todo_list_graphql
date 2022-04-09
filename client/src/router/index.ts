4c850d2dbe6060a7b91e4988306720acf147045e
rahhi555 <68116815+rahhi555@users.noreply.github.com>
2022-04-09 23:56:24 +0900

    add guard

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import { useUserStore } from '../stores/currentUser'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,13 +9,33 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+      meta: { requiresAuth: false },
     },
     {
       path: "/signup",
       name: "signup",
       component: () => import("../views/default/SignUp.vue"),
+      meta: { requiresAuth: false },
+    },
+    {
+      path: "/todos",
+      name: "todos",
+      component: () => import("../views/protected/Todos.vue"),
+      meta: { requiresAuth: true },
     },
   ],
 });
+
+const { hasCurrentUser } = useUserStore()
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if(!hasCurrentUser) {
+      next('/signup')
+    } else {
+      next()
+    } 
+  }
+})
 
 export default router;
